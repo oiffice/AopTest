1dcee4164f430380b1e25eb5656a243f4c40ee86
oiffice Tsai <oiffice90121@gmail.com>
2019-03-13 17:50:20 +0800

    Add ControllerAOP

--- src/main/java/york/test/aoptest/aop/ControllerAop.java
+++ src/main/java/york/test/aoptest/aop/ControllerAop.java
@@ -1,4 +1,30 @@
 package york.test.aoptest.aop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
 public class ControllerAop {
+
+    @Pointcut(value = "execution(* york.test.aoptest.controller.WeatherController.*(..))")
+    public void inputValidate() {}
+
+    @Around("inputValidate()")
+    public void checkCityName(ProceedingJoinPoint pj) {
+
+        Object[] objects = pj.getArgs();
+
+
+        for (Object o : objects) {
+
+
+        }
+
+    }
 }
