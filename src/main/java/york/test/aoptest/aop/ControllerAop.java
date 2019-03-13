9cc47c66136561e7c93e1d6fd11665144ebad867
oiffice Tsai <oiffice90121@gmail.com>
2019-03-13 18:12:26 +0800

    Implement ContrllerAop

--- src/main/java/york/test/aoptest/aop/ControllerAop.java
+++ src/main/java/york/test/aoptest/aop/ControllerAop.java
@@ -6,6 +6,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import york.test.aoptest.bean.ResultBean;
+import york.test.aoptest.util.CityChecker;
 
 @Aspect
 @Component
@@ -16,15 +18,41 @@ public class ControllerAop {
     public void inputValidate() {}
 
     @Around("inputValidate()")
-    public void checkCityName(ProceedingJoinPoint pj) {
+    public ResultBean checkCityName(ProceedingJoinPoint pj) {
+
+
+        log.info("Method Call: {} {}", pj.getSignature().getDeclaringType(), pj.getSignature().getName());
 
         Object[] objects = pj.getArgs();
+        ResultBean<?> resultBean;
 
+        try {
 
-        for (Object o : objects) {
+            for (Object o : objects) {
 
+                if(!CityChecker.getInstance().isValidateCity((String) o)) {
+                    throw new Throwable("Invalidate city name " + o);
+                }
 
+            }
+
+            resultBean = (ResultBean<?>) pj.proceed();
+
+        } catch (Throwable t) {
+            resultBean = handleException(t, pj);
         }
+
+        return resultBean;
+    }
+
+    private ResultBean handleException(Throwable throwable, ProceedingJoinPoint pj) {
+
+        log.error("Something wrong: {} error {}", pj.getSignature(), throwable.getMessage());
+
+        ResultBean resultBean = new ResultBean();
+        resultBean.setCode(ResultBean.FAIL);
+        resultBean.setMsg(throwable.getMessage());
+        return resultBean;
 
     }
 }
