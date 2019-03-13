10b39dc38fcb11dc783bd0153d8d703843728903
oiffice Tsai <oiffice90121@gmail.com>
2019-03-13 17:18:46 +0800

    Add pom.xml and basic code

---
+++ src/main/java/york/test/aoptest/AoptestApplication.java
@@ -0,0 +1,13 @@
+package york.test.aoptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoptestApplication.class, args);
+    }
+
+}
